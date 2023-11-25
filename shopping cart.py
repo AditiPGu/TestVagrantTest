47c7f18fd440a3b94c36667055243083188668c8
AditiPGu <151987281+AditiPGu@users.noreply.github.com>
2023-11-25 13:40:49 +0530

    Add files via upload

---
+++ shopping cart.py
@@ -0,0 +1,13 @@
+class basket:
+    def unit(price):
+        UP=int(unitprice)
+        GST=float(GST/100)
+        Q=int(quantity)
+        total=(UP+GST*Q)
+        
+        for i in range(UP>=500):
+            if (UP>=500):
+                print(UP/(5/100)*Q)
+            else:
+                print(UP=UP+GST*Q)
+        
